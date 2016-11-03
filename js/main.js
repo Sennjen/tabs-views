b63760daa0cec9ce5af3376cc875da09f1f632f3
Eugen Dzubenko <1senjen1@gmail.com>
2016-11-03 14:40:28 +0200

    Updated table view, popup with slider added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(function () {
     $(".dropdown-button").dropdown();
+    $('.modal-trigger').leanModal();
     var jqxhr = $.getJSON( "data.json", function(data) {
             if (data){
                 $("#dropdown1").empty();
-                $('.dropdown-button span').html(data.titleList[0]);
+                $('.dropdown-button span').html(data.numList[0]);
 
-                data.titleList.forEach(function (d) {
+                data.numList.forEach(function (d) {
                     $("#dropdown1").append("<li><a href='javascript:void(0)' data-title='"+ d +"'>"+ d +"</a></li>")
                 });
 
@@ -14,7 +15,7 @@ $(function () {
                     buildTable($(this).data('title'), data)
                 });
 
-                buildTable(data.titleList[0], data)
+                buildTable(data.numList[0], data)
             }
         })
         .fail(function(err) {
@@ -22,31 +23,68 @@ $(function () {
         });
 
     function buildTable (table_name, data) {
-        var table_data =  _.find(data.allData, table_name),
+        var table_data =  data.allData,
             table_head = $('table thead tr'),
             table_body = $('table tbody'),
-            col = '',
-            col_count = table_data[table_name][0].data.length + 1,
-            col_width = 100/col_count;
-            cols_names = [];
+            col_count = table_data.length + 1,
+            col_width = 100/col_count,
+            row_data = [];
 
         $("table thead tr, table tbody").empty();
 
         table_head.append('<th>Rows</th>');
-        
-        table_data[table_name].forEach(function (d,i) {
-            // console.log(d);
-            table_body.append('<tr><td style="width: '+col_width+'%">'+ d.row +'</td></tr>');
-            d.data.forEach(function (row_data) {
-                for (col in row_data) break;
-                cols_names.push(col);
-                table_body.find('tr:last-child')
-                    .append('<td '+ (row_data.good == "True" ? "class='blue accent-2'" : "class='red accent-2'") +' style="width: '+col_width+'%">'+ row_data[col] +'</td>')
-            })
+        data.rowsList.forEach(function (d,i) {
+            table_body.append('<tr class="'+d+'"><td style="width: '+col_width+'%">'+ d +'</td></tr>');
         });
-
-        _.uniq(cols_names).forEach(function (d,i) {
+        data.titleList.forEach(function (d,i) {
             table_head.append('<th class="header_cells">'+ d +'</th>');
+            data.rowsList.forEach(function (elem,index) {
+                row_data = _.find(_.find(table_data, d)[d], ['row',elem]);
+                if(row_data){
+                    table_body.find('.'+elem+'')
+                        .append('<td ' +
+                            (_.find(row_data.data, table_name).good == "True" ? "class='blue accent-2'" : "class='red accent-2'") +
+                            'data-title="'+d+'" data-row="'+elem+'"' +
+                            (_.find(row_data.data, table_name).image ? 'data-modal="true"':'data-modal="false"')+
+                            ' style="width: '+col_width+'%">'+
+                            _.find(row_data.data, table_name)[table_name]+
+                            '</td>')
+                }else{
+                    table_body.find('.'+elem+'').append('<td class="grey" style="width: '+col_width+'%"></td>')
+                }
+            });
         });
+        $('td').on('click', function (e) {
+            var cell = $(this);
+            var cell_data = [];
+            var modal_content = $('#modal1 .modal-content');
+            if(cell.data('modal')){
+                cell_data = _.find(_.find(_.find(data.allData, cell.data('title'))[cell.data('title')],
+                    ['row', cell.data('row')]).data, table_name);
+                modal_content.empty();
+                modal_content.append('<div class="slider"><ul id="lightSlider"></ul></div>');
+                cell_data.image.forEach(function (elem) {
+                    $('#lightSlider')
+                        .append('<li data-thumb="'+elem+'">'+
+                                '<img src="'+elem+'">'+
+                                '</li>')
+                });
+                modal_content.append('<div class="links-container"><span>Download Links:</span></div>');
+                cell_data.download.forEach(function (elem) {
+                    $('.links-container').append('<a href="'+elem+'" target="_blank">'+elem+'</a>')
+                });
+                $('#modal1').openModal({
+                    ready: function () {
+                        $('#lightSlider').lightSlider({
+                            gallery: true,
+                            item: 1,
+                            loop: true,
+                            slideMargin: 0,
+                            thumbItem: 3
+                        });
+                    }
+                });
+            }
+        })
     }
 });
